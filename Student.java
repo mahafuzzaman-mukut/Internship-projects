a1bbbbba35664678652c8b87b4b8fae67013bdaf
Mahafuzzaman Mukut <mahafuzzaman9mukut@gmail.com>
2026-05-03 15:35:36 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+package bd.mukut.gradetracker;
+
+public class Student {
+    String name;
+    int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
